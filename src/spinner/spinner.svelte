3ad736938d682d71517e80ad3366ea48d5464bff
Shaun Menezes <shaunshanil95@gmail.com>
2024-06-08 10:40:22 +0800

    fixed spinner

--- src/spinner/spinner.svelte
+++ src/spinner/spinner.svelte
@@ -3,7 +3,14 @@
 </script>
 
 {#if $loading}
-	<div class="loader" />
+	<div class="loader">
+		<div class="spinner"></div>
+		<div class="loading-text">
+			<p>This can take up to 1 minute. Just let it load.</p>
+			<p>You can walk to your car. It will be done by then!</p>
+			<p>THANK YOU!</p>
+		</div>
+	</div>
 {/if}
 
 <style>
@@ -14,22 +21,28 @@
 		width: 100vw;
 		height: 100vh;
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		background-color: black;
 		transition:
 			opacity 0.75s,
 			visibility 0.75s;
+		color: white; /* Ensures text is white */
 	}
 
-	.loader::after {
-		content: '';
+	.spinner {
 		width: 75px;
 		height: 75px;
 		border: 15px solid lightgrey;
 		border-top-color: darkviolet;
 		border-radius: 50%;
 		animation: loading 0.75s ease infinite;
+	}
+
+	.loading-text {
+		text-align: center;
+		margin-top: 20px; /* Space between spinner and text */
 	}
 
 	@keyframes loading {
